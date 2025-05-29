1b6d4c02886732bdde3350266bdae66278dde0e0
kyccej12 <jecky.tiongco@gmail.com>
2025-05-29 08:55:47 +0800

    Adding My projects section

--- src/Content.js
+++ src/Content.js
@@ -18,9 +18,9 @@ import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo2.png";
 import services_logo3 from "./assets/images/Services/logo3.png";
 
-import project1 from "./assets/images/projects/img1.png";
-import project2 from "./assets/images/projects/img2.png";
-import project3 from "./assets/images/projects/img3.png";
+import project1 from "./assets/images/projects/monde.png";
+import project2 from "./assets/images/projects/primecare.png";
+import project3 from "./assets/images/projects/primecaremobile.png";
 import person_project from "./assets/images/projects/person.png";
 
 import avatar1 from "./assets/images/Testimonials/avatar1.png";
@@ -234,16 +234,19 @@ export const content = {
     image: person_project,
     project_content: [
       {
-        title: "Gym Website",
+        title: "MONDE",
         image: project1,
+        readmore: "https://github.com/kyccej12/eCommerce-landing-page",
       },
       {
-        title: "Social Media web",
+        title: "Prime Care Cebu - ERP",
         image: project2,
+        readmore: "https://github.com/kyccej12/prime-care-cebu-erp",
       },
       {
-        title: "Creative Website",
+        title: "Prime Care Cebu - Mobile Clinic ERP",
         image: project3,
+        readmore: "https://github.com/kyccej12/primecare-erp-mobile",
       },
     ],
   },
