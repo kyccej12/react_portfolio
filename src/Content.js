d898f20f050f9c7d9b32b662b108fc6f74a1ba54
kyccej12 <jecky.tiongco@gmail.com>
2025-05-29 15:14:14 +0800

    Modify Content for deployment

--- src/Content.js
+++ src/Content.js
@@ -18,10 +18,10 @@ import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo2.png";
 import services_logo3 from "./assets/images/Services/logo3.png";
 
-import project1 from "./assets/images/projects/monde.png";
-import project2 from "./assets/images/projects/primecare.png";
-import project3 from "./assets/images/projects/primecaremobile.png";
-import person_project from "./assets/images/projects/person.png";
+import project1 from "./assets/images/Projects/monde.png";
+import project2 from "./assets/images/Projects/primecare.png";
+import project3 from "./assets/images/Projects/primecaremobile.png";
+import person_project from "./assets/images/Projects/person.png";
 
 import avatar1 from "./assets/images/Testimonials/avatar1.png";
 import avatar2 from "./assets/images/Testimonials/avatar2.png";
