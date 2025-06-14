f17b43499a5c77479e08ea658cd41269cf6161ce
kyccej12 <jecky.tiongco@gmail.com>
2025-06-14 13:48:33 +0800

    Fix app.jsx error

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Service from "./components/Services";
 import Projects from "./components/Projects";
 // import Testimonials from "./components/Testimonials";
 import Hireme from "./components/Hireme";
-// import Contact from "./components/Contact";
+import Contact from "./components/Contact";
 import { useEffect } from "react";
 // Animation package
 import Aos from "aos";
